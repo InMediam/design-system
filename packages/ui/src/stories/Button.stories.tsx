7e8077fb72c69f146127fdd9a2f6643fa1949bb0
Douglas <douglasduarte.eng@gmail.com>
2024-12-26 16:20:08 -0300

    refactor: #868bhy31r adiciona template para documentação do button.

--- packages/ui/src/stories/Button.stories.tsx
+++ packages/ui/src/stories/Button.stories.tsx
@@ -1,17 +1,36 @@
 import { Button, ButtonProps } from "../components/ui/button";
-import { Meta, StoryObj } from "@storybook/react";
+import { Meta, StoryFn } from "@storybook/react";
 
 export default {
   title: 'Form/Button',
   component: Button,
-
   args: {
     children: 'Confirmar',
   },
 } as Meta<ButtonProps>
 
-export const Primary: StoryObj<ButtonProps> = {
-  args: {
-    variant: 'success',
-  },
+const Template: StoryFn<typeof Button> = (args: ButtonProps) => <Button {...args} />;
+
+export const ExtraSmall: StoryFn<typeof Button> = Template.bind({});
+ExtraSmall.args = {
+  variant: 'default',
+  size: 'xs',
+};
+
+export const Small: StoryFn<typeof Button> = Template.bind({});
+Small.args = {
+  variant: 'default',
+  size: 'sm',
 }
+
+export const Medium: StoryFn<typeof Button> = Template.bind({});
+Medium.args = {
+  variant: 'default',
+  size: 'default',
+};
+
+export const Large: StoryFn<typeof Button> = Template.bind({});
+Large.args = {
+  variant: 'default',
+  size: 'lg',
+};
