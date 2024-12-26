bc7b0499d1b05ec0d74297f7f5f740792008f39f
Douglas <douglasduarte.eng@gmail.com>
2024-12-26 10:23:45 -0300

    feat: #868bc7n64 add dialog component and doc.

---
+++ packages/ui/src/stories/Dialog.stories.tsx
@@ -0,0 +1,64 @@
+import { Meta, StoryObj } from '@storybook/react'
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  DialogProps
+} from '../components/ui/dialog'
+import { Button } from '../components/ui/button'
+import { Label } from '../components/ui/label'
+import { Input } from '../components/ui/input'
+
+export default {
+  title: 'Dialog',
+} as Meta<DialogProps>
+
+export const Primary: StoryObj<DialogProps> = {
+  args: {},
+  render: () => {
+    return (
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button variant="outline">Edit Profile</Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Edit profile</DialogTitle>
+            <DialogDescription>
+              Make changes to your profile here. Click save when you're done.
+            </DialogDescription>
+          </DialogHeader>
+          <div className="grid gap-4 py-4">
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="name" className="text-right">
+                Name
+              </Label>
+              <Input
+                id="name"
+                defaultValue="Douglas Duarte"
+                className="col-span-3"
+              />
+            </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="username" className="text-right">
+                Username
+              </Label>
+              <Input
+                id="username"
+                defaultValue="@duarteDouglas"
+                className="col-span-3"
+              />
+            </div>
+          </div>
+          <DialogFooter>
+            <Button type="submit">Save changes</Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    )
+  },
+}
