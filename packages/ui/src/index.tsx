a8c6f4d7531e27f85eb77c6aa9c20c17717e94c8
Douglas <douglasduarte.eng@gmail.com>
2024-12-26 11:42:02 -0300

    feat: #868bc7n64 update components export.

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -2,5 +2,15 @@ import * as React from "react";
 
 import "../style/styles.css"
 
-// component exports
+// Component exports
 export * from "./components/ui/button";
+export * from "./components/ui/label";
+export * from "./components/ui/input";
+export * from "./components/ui/switch";
+
+export * from "./components/ui/dialog";
+export * from "./components/ui/accordion";
+export * from "./components/ui/card";
+export * from "./components/ui/tabs";
+export * from "./components/ui/chart";
+export * from "./components/ui/tooltip";
