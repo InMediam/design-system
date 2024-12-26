84329c5e68ae7e1542d44dd6bbc95ad38acb0d0d
Douglas <douglasduarte.eng@gmail.com>
2024-12-26 16:25:01 -0300

    feat: #868bhy31r update components exports

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -6,7 +6,11 @@ import "../style/styles.css"
 export * from "./components/ui/button";
 export * from "./components/ui/label";
 export * from "./components/ui/input";
+export * from "./components/ui/textarea";
+export * from "./components/ui/select";
 export * from "./components/ui/switch";
+
+export * from "./components/ui/skeleton";
 
 export * from "./components/ui/dialog";
 export * from "./components/ui/accordion";
@@ -14,3 +18,9 @@ export * from "./components/ui/card";
 export * from "./components/ui/tabs";
 export * from "./components/ui/chart";
 export * from "./components/ui/tooltip";
+export * from "./components/ui/badge";
+export * from "./components/ui/breadcrumb";
+export * from "./components/ui/carousel";
+
+export * from "./components/ui/dropdown-menu";
+export * from "./components/ui/navigation-menu";
