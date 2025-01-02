e1c8c6fede96af5e62bdf135cd02aa0975d86643
Douglas <douglasduarte.eng@gmail.com>
2025-01-02 16:08:40 -0300

    feat: #868bhy31r atualiza imports de componentes

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -1,27 +1,36 @@
 import "../style/styles.css"
 
 // Component exports
-export * from "./components/ui/button";
-export * from "./components/ui/label";
-export * from "./components/ui/input";
-export * from "./components/ui/textarea";
-export * from "./components/ui/select";
-export * from "./components/ui/switch";
-export * from "./components/ui/inputItemsWrapper";
-
-export * from "./components/ui/skeleton";
-
-export * from "./components/ui/dialog";
 export * from "./components/ui/accordion";
-export * from "./components/ui/card";
-export * from "./components/ui/tabs";
-export * from "./components/ui/chart";
-export * from "./components/ui/tooltip";
+export * from "./components/ui/avatar";
 export * from "./components/ui/badge";
 export * from "./components/ui/breadcrumb";
+export * from "./components/ui/button";
+export * from "./components/ui/calendar";
+export * from "./components/ui/card";
 export * from "./components/ui/carousel";
-export * from "./components/ui/table";
-export * from "./components/ui/avatar";
-
+export * from "./components/ui/chart";
+export * from "./components/ui/checkbox";
+export * from "./components/ui/dialog";
+export * from "./components/ui/dot";
 export * from "./components/ui/dropdown-menu";
+export * from "./components/ui/input";
+export * from "./components/ui/inputItemsWrapper";
+export * from "./components/ui/label";
+export * from "./components/ui/menubar";
 export * from "./components/ui/navigation-menu";
+export * from "./components/ui/popover";
+export * from "./components/ui/select";
+export * from "./components/ui/separator";
+export * from "./components/ui/skeleton";
+export * from "./components/ui/switch";
+export * from "./components/ui/table";
+export * from "./components/ui/tabs";
+export * from "./components/ui/textarea";
+export * from "./components/ui/tooltip";
+
+
+
+
+
+
