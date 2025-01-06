0824d0c5daeae93239d82c818a03f395bd7015e3
Douglas <douglasduarte.eng@gmail.com>
2025-01-06 13:56:53 -0300

    feat: #868bhy31r atualização dos imports.

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -11,26 +11,37 @@ export * from "./components/ui/card";
 export * from "./components/ui/carousel";
 export * from "./components/ui/chart";
 export * from "./components/ui/checkbox";
+export * from "./components/ui/collapsible";
 export * from "./components/ui/dialog";
 export * from "./components/ui/dot";
+export * from "./components/ui/drawer";
 export * from "./components/ui/dropdown-menu";
+export * from "./components/ui/input-otp";
 export * from "./components/ui/input";
 export * from "./components/ui/inputItemsWrapper";
 export * from "./components/ui/label";
 export * from "./components/ui/menubar";
 export * from "./components/ui/navigation-menu";
 export * from "./components/ui/popover";
+export * from "./components/ui/progress";
+export * from "./components/ui/radio-group";
 export * from "./components/ui/select";
 export * from "./components/ui/separator";
+export * from "./components/ui/sheet";
+export * from "./components/ui/sidebar";
 export * from "./components/ui/skeleton";
+export * from "./components/ui/slider";
 export * from "./components/ui/switch";
 export * from "./components/ui/table";
 export * from "./components/ui/tabs";
 export * from "./components/ui/textarea";
 export * from "./components/ui/tooltip";
 
+// Hook exports
+export * from "./hooks/use-mobile";
 
-
+// Utility exports
+export * from "./lib/utils";
 
 
 
