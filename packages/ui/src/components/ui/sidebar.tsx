9a8f7fa0ee6cfd4feadfe4662b17b97f62a4fbe0
Douglas <douglasduarte.eng@gmail.com>
2025-01-09 15:34:37 -0300

    fix: #868bhy31r sidebar panel icon.

--- packages/ui/src/components/ui/sidebar.tsx
+++ packages/ui/src/components/ui/sidebar.tsx
@@ -16,7 +16,7 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "./tooltip"
-import { ViewVerticalIcon } from "@radix-ui/react-icons"
+import { PanelLeft } from "lucide-react"
 
 const SIDEBAR_COOKIE_NAME = "sidebar:state"
 const SIDEBAR_COOKIE_MAX_AGE = 60 * 60 * 24 * 7
@@ -277,7 +277,7 @@ const SidebarTrigger = React.forwardRef<
       }}
       {...props}
     >
-      <ViewVerticalIcon />
+      <PanelLeft className="w-5 h-5" />
       <span className="sr-only">Toggle Sidebar</span>
     </Button>
   )
