838709096980751be85e167afa4f710f38d18d1f
Douglas <douglasduarte.eng@gmail.com>
2025-01-17 16:44:35 -0300

    feat: #868c0t1cc conteudo com gradiente linear do menu do usuário.

--- apps/web/src/pages/_layouts/components/menu-user.tsx
+++ apps/web/src/pages/_layouts/components/menu-user.tsx
@@ -1,5 +1,4 @@
 import {
-  Bell,
   ChevronsUpDown,
   LogOut,
 } from "lucide-react"
@@ -8,6 +7,7 @@ import {
   Avatar,
   AvatarFallback,
   AvatarImage,
+  Badge,
   Button,
 } from "ui"
 import {
@@ -37,21 +37,28 @@ export function MenuUser() {
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent
-        className="w-[--radix-dropdown-menu-trigger-width] min-w-56 rounded-lg"
+        className="w-[20rem] h-[31.125rem] min-w-56 rounded-lg"
         side="bottom"
         align="end"
-        sideOffset={4}
+        sideOffset={20}
       >
-        <DropdownMenuGroup>
-          <DropdownMenuItem className="flex gap-1">
-            <Bell className="w-4 h-4" />
-            Notifications
-          </DropdownMenuItem>
+        <DropdownMenuGroup className="flex flex-col justify-center items-center py-6 px-4 bg-gradient-to-b from-brand-25 from-0% to-white to-100%">
+          <Avatar className="h-10 w-10 rounded-full mb-4">
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback className="rounded-lg">DD</AvatarFallback>
+          </Avatar>
+          <div className="flex flex-col items-center leading-tight gap-[0.25rem] pb-2">
+            <span className="truncate text-sm font-semibold text-gray-950 dark:text-gray-400">Douglas Duarte</span>
+            <span className="truncate text-xs font-normal text-gray-700">douglasduarte.eng@gmail.com</span>
+          </div>
+          <Badge variant="outline">
+            Corretor(a)
+          </Badge>
         </DropdownMenuGroup>
         <DropdownMenuSeparator />
         <DropdownMenuItem className="flex gap-1">
           <LogOut className="w-4 h-4" />
-          Log out
+          Sair
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
