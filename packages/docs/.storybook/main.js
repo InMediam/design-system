563cae3ba231f7e007b962ab6130b79267a808ad
Douglas <douglasduarte.eng@gmail.com>
2025-01-22 11:58:17 -0300

    fix: configuração do storybook

---
+++ packages/docs/.storybook/main.js
@@ -0,0 +1,27 @@
+module.exports = {
+  "stories": [
+    "../src/pages/**/*.stories.mdx",
+    "../src/stories/**/*.stories.tsx"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions",
+    "@storybook/addon-a11y"
+  ],
+  "framework": "@storybook/react-vite",
+  "core": {
+    "builder": "@storybook/builder-vite"
+  },
+  "features": {
+    "storyStoreV7": true
+  },
+  viteFinal: (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
+      config.base = '/05-design-system/'
+    }
+
+    return config
+  }
+}
