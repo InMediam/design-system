3ad6b8673f1e0724a090b31792de307c605cbeed
Júlio Alves <dev.julio.alves@gmail.com>
2026-06-05 17:15:03 -0300

    fix: update badge colors

--- packages/ui/src/components/ui/badge.tsx
+++ packages/ui/src/components/ui/badge.tsx
@@ -10,13 +10,13 @@ const badgeVariants = cva(
     variants: {
       variant: {
         default:
-          "border-brand bg-brand-primary-alt text-brand-secondary hover:bg-brand-secondary dark:border-brand/30",
+          "border-brand bg-brand-primary-alt text-brand-secondary hover:bg-brand-secondary",
         secondary:
           "border-secondary bg-secondary text-secondary hover:bg-secondary-hover",
         destructive:
           "border-error-subtle bg-error-primary text-error-primary-hover hover:bg-error-secondary",
-        success: "border-secondary bg-success-primary text-success-primary hover:bg-success-secondary hover:dark:bg-success-primary/85",
-        warning: "border-warning bg-warning-secondary text-warning-primary dark:bg-primary-alt dark:border-warning/30",
+        success: "bg-success-primary text-success-primary hover:bg-success-secondary",
+        warning: "border-warning bg-warning-secondary text-warning-primary",
         outline: "text-secondary",
       },
     },
